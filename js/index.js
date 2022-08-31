f57680ff222a3cc5fdc83386051f40cd72cf7af0
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-08-31 11:41:57 +0300

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {});
+function getBooks() {
+	return fetch('http://localhost:3000/books')
+	.then (resp => resp.json())
+	}
+
+function createBookLI(bookObject) {
+	const liElement = document.createElement('li');
+	liElement.textContent = bookObject.title;
+	liElement.addEventListener("click",showBookDetails)
+	return liElement;
+	}
+
+function showBooksList(booksArray) {
+	const LIContainer = document.getElementById('list');
+	booksArray.forEach(bookObject => {
+		const bookLI = createBookLI(bookObject);
+		LIContainer.appendChild(bookLI);
+		});
+	};
+
+function showBookDetails() {
+	alert(event.target.innerText);
+	}
+
+getBooks().then(showBooksList);
